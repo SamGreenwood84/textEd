2d57c52ff64b997a23083f771625824bf25c1d8f
your-SamGreenwood <your-sagg_84@hotmail.com>
2024-04-12 16:19:06 -0300

    debugging deployment errors

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Serve static files from the 'client/dist' directory
-app.use(express.static(path.join(__dirname, '..', 'client', 'dist')));
+app.use(express.static(path.join(__dirname, 'client', 'dist')));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
