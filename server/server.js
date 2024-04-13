1aa43cab07bc8cc8d468b88865c01b61c3b48cd2
your-SamGreenwood <your-sagg_84@hotmail.com>
2024-04-13 20:37:16 -0300

    removed req path

--- server/server.js
+++ server/server.js
@@ -1,18 +1,17 @@
 const express = require('express');
-const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Serve static files from the 'client/dist' directory
-app.use(express.static(path.join(__dirname, 'client', 'dist')));
+app.use(express.static(__dirname + '/client/dist'));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Route for serving the index.html file
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+  res.sendFile(__dirname + '/client/dist/index.html');
 });
 
 app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
