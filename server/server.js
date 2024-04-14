be95ceb3af5556a455f2a7384280ee0c539bb64c
your-SamGreenwood <your-sagg_84@hotmail.com>
2024-04-13 21:00:31 -0300

    corrected server

--- server/server.js
+++ server/server.js
@@ -4,14 +4,11 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Serve static files from the 'client/dist' directory
-app.use(express.static(__dirname + '/client/dist'));
+app.use(express.static('../client/dist'));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Route for serving the index.html file
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/client/dist/index.html');
-});
+require('./routes/htmlRoutes')(app);
 
 app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
